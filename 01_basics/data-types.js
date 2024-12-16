e19cf2dac96df68fe85a2820c3caf7b4240349e9
Raj <gdeepraj058@gmail.com>
2024-12-16 14:18:35 +0000

    data types

---
+++ 01_basics/data-types.js
@@ -0,0 +1,25 @@
+"use scrict" //treat all code newer version
+
+// alert(3+3) // we are using node.js, not browser
+
+let age = 13
+let name = "Hitesh"
+
+/* data types:-
+1.primitive
+-> number range = 2^53
+-> bigint
+-> string (prefer "" over '')
+-> boolean : true/false
+-> null : standalone value
+-> undefined
+-> symbol: uniqueness
+
+
+2.
+-> object : very imp.
+*/
+console.log(typeof "Hitesh")
+console.log(typeof age)
+console.log(typeof null) //object, some say that this is lang error some say that this is correct.
+console.log(typeof undefined) //undefined -> is js a type
