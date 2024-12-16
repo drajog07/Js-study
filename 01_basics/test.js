d5da771f6d0cba6c3ee858ca5136bba3f64223a8
Raj <gdeepraj058@gmail.com>
2024-12-16 13:20:01 +0000

    1st chap complete

--- 01_basics/test.js
+++ 01_basics/test.js
@@ -1,1 +1,11 @@
-console.log("Raj")
+const accountId = 1445553
+let accountEmail = "raj@gmail.com"
+var accountPassword = "56852"
+accountCity = "Jaipur"
+let accountState  // it will show undefined in print
+
+// accountId = 58695 -> not valid
+accountEmail = "deep@iiitb.ac.in"
+accountPassword = "89310"
+accountCity = "Bengaluru"
+console.table([accountEmail,accountId,accountPassword,accountCity, accountState])
