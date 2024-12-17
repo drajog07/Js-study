dc51c8b00f45e7ccd49bf8cac31e741706ff9aa3
Raj <gdeepraj058@gmail.com>
2024-12-17 10:54:42 +0000

    operators

--- 01_basics/dt_conversion_op.js
+++ 01_basics/dt_conversion_op.js
@@ -22,8 +22,29 @@ number other than 0 -> true
 Sting other than "" (empty string) -> true*/
 
 
-let someNumber = 33
+// let someNumber = 33
 
-let str = String(someNumber)
-console.log(typeof str)  // String
-console.log(str) // 33
+// let str = String(someNumber)
+// console.log(typeof str)  // String
+// console.log(str) // 33
+
+// console.log(2+3) -> 5
+// console.log(2-3) -> -1
+// console.log(2*3) -> 6
+// console.log(2/3) -> 0.666...
+// console.log(2%3) -> 2
+// console.log(2**3) -> 8
+
+// str1 = "Hitesh"
+// str2 = " Raj"
+// str3 = str1 + str2
+// console.log(str3)
+// console.log(2 + "3") // 23
+// console.log("2" + 3) // 23
+// console.log("2" + "3") // 23
+// console.log("2" + 1 + 2) // 212 -> whichever is 1st treated as 1st
+// console.log(2 + 1 + "2") // 32
+
+// console.log(true) //true
+// console.log(+true) // 1
+// console.log(true+) // error
