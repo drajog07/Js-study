4e5134e3538f7400d5a8d014f5c7f62d1ad763b6
Raj <gdeepraj058@gmail.com>
2024-12-20 10:21:11 +0000

    strings

---
+++ 01_basics/strings.js
@@ -0,0 +1,37 @@
+const name = "hitesh"
+const repoCount = 50
+
+// console.log(name + repoCount + " Value"); not good practice
+
+//console.log(`Hello my name is ${name} and my repo count is ${repoCount}`); // use of string interpolation always a good practice
+
+//const gameName = new String('hitesh-hc-com') // one more way to define String 
+
+// console.log(gameName[0]); // best way to 
+// console.log(gameName.__proto__);
+
+
+// console.log(gameName.length);
+ //console.log(gameName.toUpperCase());
+// console.log(gameName.charAt(2));
+// console.log(gameName.indexOf('t'));
+
+// const newString = gameName.substring(0, 4) 4-> excluded, if u pass -ve then it ignores and start auto from 0
+// console.log(newString);
+
+// const anotherString = gameName.slice(-8, 4) // can give -ve value as well
+// console.log(anotherString);
+
+// const newStringOne = "   hitesh    "
+// console.log(newStringOne);
+// console.log(newStringOne.trim());
+
+// const url = "https://hitesh.com/hitesh%20choudhary"
+
+// console.log(url.replace('%20', '-'))
+
+// console.log(url.includes('sundar'))
+
+// console.log(gameName.split('-')); // split it on basis of '-' this sign i.e. break from this sign 
+
+
